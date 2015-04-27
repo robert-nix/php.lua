8f098839260eb14fe12f248e7b763f4153350c4c
Robert Nix <mischanix@gmail.com>
2015-04-26 22:34:53 -0500

    Update runner

--- src/php.lua
+++ src/php.lua
@@ -1,56 +1,62 @@
-package.path = '?.lua;src/?.lua'
+package.path = package.path .. ';?.lua;src/?.lua'
 
 io = require'io'
 inspect = require'inspect'
 local tokenizer = require'tokenizer'
 
-local function dump(x)
+function dump(x)
 	io.write(inspect(x) .. '\n')
 end
 
 local args = {...}
---[[
-local script_file = io.open(args[1], "r")
-local script_buf = script_file:read("*a")
-local tokenized = tokenizer.script:match(script_buf)
-dump(tokenized)
-last_idx = 0
-for i,j in ipairs(tokenized) do
-	last_idx = j[2]
-end
-if last_idx ~= 1 + script_buf:len() then
-	error("tokenizer didn't reach eof")
-end
-]]
--- --[[
-local split = {}
-local i0 = 1
-local list = args[1]
-local listlen = list:len()
-for i=1,listlen do
-	if list:sub(i,i) == '\n' then
-		table.insert(split, list:sub(i0, i - 1))
-		i0 = i + 1
+if args[1] ~= 'batch' then
+	local context = { file_name = args[2] }
+	local script_file = io.open(args[2], "r")
+	local script_buf = script_file:read("*a")
+	context.file_buf = script_buf
+	local tokenized = tokenizer.script:match(script_buf)
+	local tree = require("astbuilder").build_ast(context, tokenized)
+	dump(tree)
+	last_idx = 0
+	for i,j in ipairs(tokenized) do
+		last_idx = j[2]
 	end
-	if i == listlen then
-		table.insert(split, list:sub(i0, i))
+	if last_idx ~= 1 + script_buf:len() then
+		error("tokenizer didn't reach eof")
 	end
-end
-for x,arg in ipairs(split) do
-	local status, err = pcall(function()
-		local script_file = io.open(arg, "r")
-		local script_buf = script_file:read("*a")
-		local tokenized = tokenizer.script:match(script_buf)
-		last_idx = 0
-		for i,j in ipairs(tokenized) do
-			last_idx = j[2]
+else
+	local split = {}
+	local i0 = 1
+	local list = args[2]
+	local listlen = list:len()
+	for i=1,listlen do
+		if list:sub(i,i) == '\n' then
+			table.insert(split, list:sub(i0, i - 1))
+			i0 = i + 1
 		end
-		if last_idx ~= 1 + script_buf:len() then
-			error("tokenizer didn't reach eof")
+		if i == listlen then
+			table.insert(split, list:sub(i0, i))
 		end
-	end)
-	if not status then
-		io.write(string.format("error tokenizing %s: %s\n", arg, err))
+	end
+	for x,arg in ipairs(split) do
+		io.write(arg .. '   ')
+		io.flush()
+		local status, err = pcall(function()
+			local script_file = io.open(arg, "r")
+			local script_buf = script_file:read("*a")
+			local tokenized = tokenizer.script:match(script_buf)
+			last_idx = 0
+			for i,j in ipairs(tokenized) do
+				last_idx = j[2]
+			end
+			if last_idx ~= 1 + script_buf:len() then
+				error("tokenizer didn't reach eof")
+			end
+			local tree = require("astbuilder").build_ast(tokenized)
+		end)
+		io.write('\r')
+		if not status then
+			io.write(string.format("error tokenizing %s: %s\n", arg, err))
+		end
 	end
 end
--- ]]
